15dce01a02d6f31d5582208081a405151bb40fd3
siddharthraopotukuchi <siddhartharaopotukuchi@gmail.com>
2020-12-18 12:09:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
+// NodeApp
 const app = express();
 app.get('/', (req, res) => {
   res.send('<h1 style="color:green;">Java Home App - version-10!!</h1> \n');
